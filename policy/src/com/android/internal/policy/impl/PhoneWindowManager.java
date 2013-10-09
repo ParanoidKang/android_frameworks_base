c6e16cd2c299feee9b34a3c362c069af800a6594
xilence2210 <xilence2210@gmail.com>
2013-10-09 13:28:12 +0800

    PhoneWindowManager: Fix duplicate number
    Change-Id: I271988a8f205a97456edccc202d93962d45cf7c8

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -183,7 +183,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int KEY_ACTION_VOICE_SEARCH = 4;
     private static final int KEY_ACTION_IN_APP_SEARCH = 5;
     private static final int KEY_ACTION_SLEEP = 6;
-    private static final int KEY_ACTION_LAUNCH_CAMERA = 6;
+    private static final int KEY_ACTION_LAUNCH_CAMERA = 7;
 
     // Masks for checking presence of hardware keys.
     // Must match values in core/res/res/values/config.xml
